912c9c617059cd468b091a70dcee0656e0d778c0
ahmedyosef <33419399+ahmedyosef@users.noreply.github.com>
2025-03-02 17:27:46 +0200

    final
    final

--- Assets/Script/RoomControler.cs
+++ Assets/Script/RoomControler.cs
@@ -8,46 +8,94 @@ public class RoomControler : MonoBehaviour
     public Sprite sprite;
     public string[] roomNames;
     public GameObject linRender;
-    public Transform parentObject; // Reference to the parent GameObject
-    public Transform[] points;
+    public Transform parentObjectOfRoom; // Reference to the parent GameObject
+    public Transform[] Roompoints;
     public CalculatePath calculatePath;
     public int roomNumper;
     public DynamicCamera Camera;
 
+    public TMP_Dropdown dropDownServices;
+    public Transform parentObjectOfServices; // Reference to the parent GameObject
+    public Transform[] servicespoints;
+    public string[] servicesNames;
+
     private void Start()
     {
+        roomData();
+        servicesData();
+    }
+    public void roomData()
+    {
         // Get all child GameObjects from the parent and store them in the array
-        points = new Transform[parentObject.childCount];
+        Roompoints = new Transform[parentObjectOfRoom.childCount];
 
-        for (int i = 0; i < parentObject.childCount; i++)
+        for (int i = 0; i < parentObjectOfRoom.childCount; i++)
         {
-            points[i] = parentObject.GetChild(i).transform;
+            Roompoints[i] = parentObjectOfRoom.GetChild(i).transform;
         }
         // Initialize the roomNames array with the same length as points
-        roomNames = new string[points.Length];
+        roomNames = new string[Roompoints.Length];
 
-        for (int i = 0; i < points.Length; i++)
+        for (int i = 0; i < Roompoints.Length; i++)
         {
-            roomNames[i] = points[i].name;
+            roomNames[i] = Roompoints[i].name;
         }
         dropDownRoom.options.Clear();
 
         // Create a list of OptionData for the dropdown
         List<TMP_Dropdown.OptionData> options = new List<TMP_Dropdown.OptionData>();
         //options.Add(new TMP_Dropdown.OptionData("Rooms", sprite, Color.white));
-        for (int i = 0;i < roomNames.Length; i++) 
+        for (int i = 0; i < roomNames.Length; i++)
         {
-            options.Add(new TMP_Dropdown.OptionData(roomNames[i], sprite,Color.white));
+            options.Add(new TMP_Dropdown.OptionData(roomNames[i], sprite, Color.white));
         }
 
         dropDownRoom.AddOptions(options);
     }
     public void Room()
     {
-        roomNumper=dropDownRoom.value;
+        dropDownServices.value = 0;
+        roomNumper= dropDownRoom.value;
         linRender.SetActive(true);
-        calculatePath.destinationObject = points[roomNumper];
-        Camera.EndPoint= points[roomNumper];
+        calculatePath.destinationObject = Roompoints[roomNumper];
+        Camera.EndPoint= Roompoints[roomNumper];
+    }
+
+    void servicesData()
+    {
+        // Get all child GameObjects from the parent and store them in the array
+        servicespoints = new Transform[parentObjectOfServices.childCount];
+
+        for (int i = 0; i < parentObjectOfServices.childCount; i++)
+        {
+            servicespoints[i] = parentObjectOfServices.GetChild(i).transform;
+        }
+        // Initialize the servicesNames array with the same length as points
+        servicesNames = new string[servicespoints.Length];
+
+        for (int i = 0; i < servicespoints.Length; i++)
+        {
+            servicesNames[i] = servicespoints[i].name;
+        }
+        dropDownServices.options.Clear();
+
+        // Create a list of OptionData for the dropdown
+        List<TMP_Dropdown.OptionData> options = new List<TMP_Dropdown.OptionData>();
+        //options.Add(new TMP_Dropdown.OptionData("Rooms", sprite, Color.white));
+        for (int i = 0; i < servicesNames.Length; i++)
+        {
+            options.Add(new TMP_Dropdown.OptionData(servicesNames[i], sprite, Color.white));
+        }
+
+        dropDownServices.AddOptions(options);
+    }
+    public void Services()
+    {
+        dropDownRoom.value = 0;
+        roomNumper = dropDownServices.value;
+        linRender.SetActive(true);
+        calculatePath.destinationObject = servicespoints[roomNumper];
+        Camera.EndPoint = servicespoints[roomNumper];
     }
 }
 
